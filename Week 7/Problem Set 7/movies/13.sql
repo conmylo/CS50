5e89ac93f1ec45bc1c737a830c6756c9141a5f3d
con <75327834+conmylo@users.noreply.github.com>
2022-11-17 15:35:17 +0200

    Add files via upload

---
+++ Week 7/Problem Set 7/movies/13.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT name
+FROM stars JOIN people ON people.id = stars.person_id
+WHERE movie_id IN (SELECT movie_id FROM people JOIN stars ON people.id = stars.person_id WHERE name = 'Kevin Bacon' and birth = 1958)
+AND name != 'Kevin Bacon';
